7cce710b383b69bc24869cb6872bb254ad722728
Ángel Arias <angadominguez96@gmail.com>
2023-08-01 17:20:59 +0200

    initial commit + home page

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const port = 3000;
+
+app.use(express.static("public"))
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get('*', (req, res, next) => {
+    res.sendFile(__dirname + "/views/404.html")
+})
+
+app.listen(port, () => {
+    console.log("Funciona en server")
+})
